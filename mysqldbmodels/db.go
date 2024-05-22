8ce135f298fc67bf36ef03fcbe27c599e88d1c1d
Prashob <89034675+9500073161@users.noreply.github.com>
2024-05-22 15:24:43 +0200

    Docker:V2
    CREATED DOCKER COMPOSE, ADDED TABLE CREATION SCRIPT.

--- mysqldbmodels/db.go
+++ mysqldbmodels/db.go
@@ -4,6 +4,7 @@ package mysqldbmodels
 
 import (
 	"errors"
+	"fmt"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -14,24 +15,25 @@ type DBClient struct {
 	Conn *gorm.DB
 }
 
-func InitializeDatabase() (*DBClient,error) {
+func InitializeDatabase() (*DBClient, error) {
 
 	dbUser := os.Getenv("DB_USER")
-    dbPassword := os.Getenv("DB_PASSWORD")
-    if dbUser == "" || dbPassword == "" {
-        return nil, errors.New("DB_USER or DB_PASSWORD environment variable not set")
-    }
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	if dbUser == "" || dbPassword == "" {
+		return nil, errors.New("DB_USER or DB_PASSWORD environment variable not set")
+	}
 
-	
 	// Connect to MySQL database
-	dsn := dbUser + ":" + dbPassword + "@tcp(host.docker.internal:3306)/coursemanagement?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	var err error
 	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-        return nil, err
-    }
+		return nil, err
+	}
 	client := &DBClient{Conn: DB}
-	return client,err
+	return client, err
 
-	
 }
